29ae19bd0ee061053e3fb9a1aad5a2f71fae284f
tuannn0107 <tuannn0107@gmail.com>
2018-10-04 22:34:30 +0700

    fix data train path

--- src/main/java/com/hvtien/utils/Constants.java
+++ src/main/java/com/hvtien/utils/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
     public static int COLOR_FOR_PAINT_RGB = Color.BLACK.getRGB();
     public static Color COLOR_FOR_BOUND = Color.RED;
     public static final String FILE_DATA_TRAIN = "data-train.json";
-    public static final String RESOURCES_PATH = "src/main/resources/";
+    public static final String RESOURCES_PATH = "";
     public static final Dimension DEFAULT_SIZE = new Dimension(300, 300);
     public static final Dimension BINARY_LAYER = new Dimension(20, 20);
     public static final int BINARY_LAYER_SCALE = 12;
